8f39316fcabac98cd0dc4dada9949d4d3dbb63e6
linglingluoluo <44063429+linglingluoluo@users.noreply.github.com>
2026-05-26 22:24:22 +0800

    cache for shop and shop type

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -1,9 +1,15 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Shop;
 import com.hmdp.mapper.ShopMapper;
 import com.hmdp.service.IShopService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.RedisConstants;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 /**
@@ -16,5 +22,53 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IShopService {
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
 
+    @Override
+    public Result queryById(Long id) {
+        //1.从redis查询商铺缓存
+        String key = RedisConstants.CACHE_SHOP_KEY + id;
+        String shopJson = stringRedisTemplate.opsForValue().get(key);
+
+        if (StrUtil.isNotBlank(shopJson)) {
+            //存在，返回
+            //为什么返回shop而不直接返回shopJson?
+            //返回 shopJson（String）：Spring 会把它当作普通字符串处理，导致前端收到的是 "{\"id\":1}" 这样的转义字符串
+            //返回 shop（Object）：Spring 会调用 JSON 序列化器，输出标准的 JSON 对象
+            // 返回 shopJson 的实际响应：
+            //{
+            //    "code": 200,
+            //    "data": "{\"id\":1,\"name\":\"店铺名\",\"address\":\"地址\"}"
+            //    // ↑ 这是字符串，前端需要 JSON.parse(data) 两次
+            //}
+            //
+            // 返回 shop 的实际响应：
+            //{
+            //    "code": 200,
+            //    "data": {
+            //        "id": 1,
+            //        "name": "店铺名",
+            //        "address": "地址"
+            //    }
+            //    // ↑ 这是对象，前端可以直接 data.id 使用
+            //}
+            Shop shop = JSONUtil.toBean(shopJson, Shop.class);
+            return Result.ok(shop);
+        }
+
+        //不存在，根据id查询你数据库
+        Shop shop=getById(id);
+
+        if(shop==null){
+            //不存在，返回错误信息
+            return Result.fail("店铺不存在");
+        }
+
+        //然后再写入redis
+        stringRedisTemplate.opsForValue().set(key, JSONUtil.toJsonStr(shop));
+
+        //返回
+        return Result.ok(shop);
+    }
 }
--- src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopTypeServiceImpl.java
@@ -1,14 +1,23 @@
 package com.hmdp.service.impl;
 
+import cn.hutool.core.collection.CollectionUtil;
+import cn.hutool.json.JSONUtil;
+import com.hmdp.dto.Result;
 import com.hmdp.entity.ShopType;
 import com.hmdp.mapper.ShopTypeMapper;
 import com.hmdp.service.IShopTypeService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.hmdp.utils.RedisConstants;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Set;
 
 /**
  * <p>
- *  服务实现类
+ * 服务实现类
  * </p>
  *
  * @author 虎哥
@@ -16,5 +25,43 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class ShopTypeServiceImpl extends ServiceImpl<ShopTypeMapper, ShopType> implements IShopTypeService {
+    @Autowired
+    private StringRedisTemplate stringRedisTemplate;
+
+    @Override
+    public Result queryTypeList() {
+        // opsForList写法
+        //
+        String key = RedisConstants.CACHE_SHOP_TYPE_KEY;
+        // 1. 从Redis查询 商铺类型缓存 , end:-1 表示取全部数据
+        List<String> shopTypeJsonList = stringRedisTemplate.opsForList().range(key, 0, -1);
+        // 2. 有就直接返回
+        if (CollectionUtil.isNotEmpty(shopTypeJsonList)) {
+            // JSON字符串转对象返回
+            return Result.ok(
+                    shopTypeJsonList.stream()
+                            .map(shopTypesJson -> JSONUtil.toBean(shopTypesJson, ShopType.class))
+                            .toList()
+            );
+        }
+        // 3. 没有就向数据库查询 MP的query()拿来用
+        List<ShopType> shopTypes = query().orderByAsc("sort").list();
+        // 4. 不存在，返回错误
+        if (CollectionUtil.isEmpty(shopTypes)) {
+            return Result.fail("商铺类型不存在...");
+        }
+        // 5. 存在， 写入Redis，这里使用Redis的List类型，String类型，就是直接所有都写在一起，对内存开销比较大。
+        // 要将List中的每个元素(元素类型ShopType) ，每个元素都要单独转成JSON，使用stream流的map映射
+        // Hutools里的 BeanUtil.copyToList 本来想模仿UserService中的写法，
+        // 传入一个CopyOptions的，但是setFieldValueEditor貌似只对beanToMap有效
+        // 改用流的形式转换每个list元素
+        List<String> shopTypesJson = shopTypes.stream()
+                .map(JSONUtil::toJsonStr)
+                .toList();
+        // 因为从数据库读出来的时候已经是按照顺序读出来的，这里想要维持顺序必须从右边push，类似队列
+        stringRedisTemplate.opsForList().rightPushAll(key, shopTypesJson);
+        // 5. 返回
+        return Result.ok(shopTypes);
+    }
 
 }
